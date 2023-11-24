64c1e14f7e06acf3818409b3a9e1d142183a4de2
majerock <33581400+majerock@users.noreply.github.com>
2023-11-25 00:48:26 +0300

    add 1.6
    Сделано ДЗ 1.6

---
+++ Razdel_1/urok6/zadanie1_6.cpp
@@ -0,0 +1,14 @@
+//
+// Created by sasha on 25.11.2023.
+//
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int a;
+    std::cout << "Vvedite TSELOE chislo:" ;
+    std::cin >> a;
+    if (a%2==0) std::cout<< "chetnoye";
+    else std::cout<< "NEchetnoye";
+};
