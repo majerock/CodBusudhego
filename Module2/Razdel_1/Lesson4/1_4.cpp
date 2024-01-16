0e75ada652837fa3284563af4feb10cb982f92bc
majerock <33581400+majerock@users.noreply.github.com>
2024-01-16 20:13:56 +0300

    Update 1_4.cpp

--- Module2/Razdel_1/Lesson4/1_4.cpp
+++ Module2/Razdel_1/Lesson4/1_4.cpp
@@ -6,20 +6,20 @@
 #include <iostream>
 
 int main() {
-    int a[3]; // объявляем массив 'a' из трех элементов
+    int q[3]; // объявляем массив 'q' из трех элементов
     int low=37767;
     int lowi=0;
     std::cout << "Enter 3 number by space: ";
     for (int i = 0; i < 3; i++) {
-        std::cin >> a[i]; // заполняем массив 'a' с клавиатуры
+        std::cin >> q[i]; // заполняем массив 'a' с клавиатуры
     }
 
 
     for (int i = 0; i < 3; i++) {
-        if  (a[i] < low) {low = a[i] ; lowi=i+1;}
+        if  (q[i] < low) {low = q[i] ; lowi=i+1;}
         else {low = low;};
     }
-        std::cout << "Low element a[" << lowi <<"]=" << low;
+        std::cout << "Low element q[" << lowi <<"]=" << low;
 
     return 0;
 }
