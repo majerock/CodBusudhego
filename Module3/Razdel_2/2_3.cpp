826979b713c9999dd6e1c6b7f1c8b1caf0634bdf
majerock <33581400+majerock@users.noreply.github.com>
2024-03-22 19:10:12 +0300

    Update 2_3.cpp

--- Module3/Razdel_2/2_3.cpp
+++ Module3/Razdel_2/2_3.cpp
@@ -1,3 +1,20 @@
 //
 // Created by sasha on 13.03.2024.
-//
+//Написать программу, в которой используется функция f() с рекурсивным вызовом.
+// При помощи данной функции необходимо найти факториал числа 3 и вывести его на консоль.
+#include <iostream>
+
+int factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int result = factorial(3);
+
+    std::cout << "Factorial number 3 =: " << result << std::endl;
+
+    return 0;
+}
