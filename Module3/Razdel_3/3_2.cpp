4a983812519ad968dca46ad0e626da1a874aea51
majerock <33581400+majerock@users.noreply.github.com>
2024-04-11 16:19:07 +0300

    Update 3_2.cpp

--- Module3/Razdel_3/3_2.cpp
+++ Module3/Razdel_3/3_2.cpp
@@ -1,3 +1,30 @@
 //
 // Created by sasha on 22.03.2024.
 //
+
+#include <iostream>
+#include <string>
+
+// класс New
+class New {
+private:
+    std::string name;
+    std::string surname;
+
+public:
+    // Конструктор класса New
+    New(const std::string& n, const std::string& s) : name(n), surname(s) {}
+
+    // Методы для доступа к переменным класса
+    std::string getName() const { return name; }
+    std::string getSurname() const { return surname; }
+};
+
+int main() {
+    // Инициализация объекта класса New
+    New myNew("John", "Doe");
+
+    std::cout << "Name: " << myNew.getName() << std::endl;
+    std::cout << "Surname: " << myNew.getSurname() << std::endl;
+
+}
